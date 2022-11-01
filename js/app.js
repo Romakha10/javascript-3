88abad8f2d5663a3c891272afe430f2e6a038d88
Romakha10 <makharadzer1@gmail.com>
2022-10-31 17:35:43 -0700

    2

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// დავალება N1
+
+const currentDay = new Date();
+const day = currentDay.getDay();
+
+switch (currentDay) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+console.log(currentDay, day);
+
+// დავალება N2
+
+let i = 0;
+for (let i = 0; i < 50; i++) {
+  console.log(i);
+}
